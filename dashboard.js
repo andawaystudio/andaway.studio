490c2a020ea1ee11b42015ba95a4da2a55beb20b
andawaystudio <108935344+andawaystudio@users.noreply.github.com>
2022-07-08 13:08:11 +0100

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,36 @@
+// Draggable
+$(function() {
+	$('.draggable').draggable({ 
+  	cancel: '.no-drag'
+	});
+});
+
+// Clock
+(function () {
+	const clockTime = document.querySelector('.clock__time');
+  const clockIcon = document.querySelector('.clock__icon');
+
+  function updateClock () {
+  	const getDate = new Date();
+  	const getTime = getDate.toLocaleTimeString('en-GB', {hour: '2-digit', minute: '2-digit'});
+    const getHr = getDate.getHours();
+    
+    clockTime.innerHTML = getTime;
+    
+    if (getHr >= 7 && getHr <= 19) {
+    	clockIcon.style.backgroundColor = 'red';
+    } else {
+    	clockIcon.style.backgroundColor = 'blue';
+    }
+  }
+
+  setInterval(function () {
+      updateClock();
+  }, 1000);
+}());
+
+// Accordion
+$('.accordion__toggles').on('click', function() {
+  $(this).siblings('.accordion__list').height($($(this).siblings('.accordion__list p')).height());
+  $('.accordion__list').not($(this).siblings('.accordion__list')).removeClass('cc--open');
+});
